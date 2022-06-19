451077157eb3ba967320dfec8098f1394a48521d
Pioneer93 <alexhazov93@rambler.ru>
2022-06-19 11:55:32 +0300

    DZ №1

---
+++ src/HomeWork01/Person.java
@@ -0,0 +1,26 @@
+package HomeWork01;
+
+public class Person {
+    private String name;
+    private int age;
+    private int maxSwimming;
+    private int maxDistance;
+
+    public Person (String name, int age, int maxSwimming, int maxDistance)
+    {
+        this.name = name;
+        this.age = age;
+        this.maxSwimming = maxSwimming;
+        this.maxDistance = maxDistance;
+    }
+    public String getName() {return name;}
+
+    public int getAge() {return age;}
+
+    public int getMaxSwimming (){return maxSwimming;}
+
+    public int getMaxDistance() {
+        return maxDistance;
+    }
+
+}
